b09f1cfe77140ee124a5f14ef92b1db9f4629312
Olga <miarik@yandex.ru>
2022-08-07 04:01:40 -0400

    Even and uneven numbers

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,10 +2,9 @@
 int numberA = Convert.ToInt32(Console.ReadLine());
 int numberB = Convert.ToInt32(Console.ReadLine());
 int nMin;
+int nMax;
 int maximum;
 int minimum;
-
-
 
 double middle = (numberA + numberB)/2;
 if (numberA > numberB) {
@@ -16,10 +15,26 @@ else {
     maximum = numberB;
     minimum = numberA;
 }
-
 nMin = minimum;
-while (nMin < middle) {
-
+while (nMin <= middle) {
+    if (nMin %2 != 0) {
+        Console.Write("Нечетное число от минимума до середины ");
+        Console.WriteLine(nMin);
+        nMin = nMin + 2;
+    }
+    else {
+        nMin++;
+    }
 }
-
+nMax = maximum;
+while (nMax >= middle) {
+    if (nMax % 2 == 0) {
+        Console.Write("Четное число от середины до максимума ");
+        Console.WriteLine(nMax);
+        nMax = nMax - 2;
+    }
+    else {
+        nMax--;
+    }
+}
 
