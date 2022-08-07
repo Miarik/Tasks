433b022395d510ac3ff70b492aa66632b31790b6
Olga <miarik@yandex.ru>
2022-08-07 02:48:48 -0400

    Initial commit

---
+++ Task/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите два разных положительных числа через Enter");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int nMin;
+int maximum;
+int minimum;
+
+
+
+double middle = (numberA + numberB)/2;
+if (numberA > numberB) {
+    maximum = numberA;
+    minimum = numberB;
+}
+else {
+    maximum = numberB;
+    minimum = numberA;
+}
+
+nMin = minimum;
+while (nMin < middle) {
+
+}
+
+
